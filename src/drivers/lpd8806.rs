47c981af5dae791f3b4eae21285c7381d413ab57
0xb-s <145866191+0xb-s@users.noreply.github.com>
2024-09-17 06:15:12 -0700

    Update lpd8806.rs

--- src/drivers/lpd8806.rs
+++ src/drivers/lpd8806.rs
@@ -1,64 +1,59 @@
+use crate::color_order::ColorOrder;
 use crate::drivers::LedDriver;
+use crate::encoding::encode_rgbw8_to_spi_data;
+use crate::encoding::RGBW8;
 use embedded_hal_async::spi::SpiBus;
 use heapless::Vec;
-use smart_leds::RGB8;
 
-pub struct Lpd8806<SPI: SpiBus<u8>, const N: usize> {
+pub struct Sk6812<SPI: SpiBus<u8>, const N: usize> {
     spi: SPI,
+    color_order: ColorOrder,
     buffer: Vec<u8, N>,
 }
 
-const START_FRAME_SIZE: usize = 4;
-const LED_FRAME_SIZE: usize = 3;
-
-impl<SPI: SpiBus<u8>, const N: usize> Lpd8806<SPI, N> {
-    /// Creates a new LPD8806 driver with the given SPI bus.
+impl<SPI: SpiBus<u8>, const N: usize> Sk6812<SPI, N> {
+    /// Creates a new SK6812 driver with the given SPI bus.
+    /// Colors default to RGB order.
     pub fn new(spi: SPI) -> Self {
         Self {
             spi,
+            color_order: ColorOrder::RGB,
             buffer: Vec::new(),
         }
     }
 
+    /// Sets the color order if not RGB.
+    pub fn set_color_order(&mut self, color_order: ColorOrder) {
+        self.color_order = color_order;
+    }
+
     /// Initializes the buffer with the required size for the given number of LEDs.
     fn initialize_buffer(&mut self, num_leds: usize) {
-        let end_frame_size = (num_leds + 31) / 32;
-        let total_size = START_FRAME_SIZE + num_leds * LED_FRAME_SIZE + end_frame_size;
+        let total_size = num_leds * 16;
 
-        // Ensure buffer is cleared and resized based on the number of LEDs
+        // Resize the buffer based on the number of LEDs
         self.buffer.clear();
-        self.buffer.resize(total_size, 0).ok(); // Resize the buffer, handle potential error silently
+        self.buffer.resize(total_size, 0).ok();
     }
 }
 
-impl<SPI: SpiBus<u8>, const N: usize> LedDriver<RGB8> for Lpd8806<SPI, N> {
+impl<SPI: SpiBus<u8>, const N: usize> LedDriver<RGBW8> for Sk6812<SPI, N> {
     type Error = SPI::Error;
 
-    async fn write(&mut self, colors: &[RGB8]) -> Result<(), Self::Error> {
+    async fn write(&mut self, colors: &[RGBW8]) -> Result<(), Self::Error> {
         let num_leds = colors.len();
 
-        // Initialize the buffer with the proper size based on the number of LEDs
+        // Initialize the buffer with the appropriate size based on the number of LEDs
         self.initialize_buffer(num_leds);
 
-        // Start frame
-        self.buffer[..START_FRAME_SIZE].fill(0x00);
+        // Encode colors into the buffer
+        encode_rgbw8_to_spi_data(&colors[..num_leds], self.color_order, &mut self.buffer[..]);
 
-        // LED frames
-        for (i, &RGB8 { r, g, b }) in colors.iter().enumerate().take(num_leds) {
-            let offset = START_FRAME_SIZE + i * LED_FRAME_SIZE;
-            // Each color is 7 bits with the highest bit set to 1
-            self.buffer[offset] = (g >> 1) | 0x80;
-            self.buffer[offset + 1] = (r >> 1) | 0x80;
-            self.buffer[offset + 2] = (b >> 1) | 0x80;
-        }
+        // Write the data to SPI
+        self.spi.write(&self.buffer[..]).await?;
 
-        // End frame
-        let end_frame_size = (num_leds + 31) / 32;
-        let end_frame_offset = START_FRAME_SIZE + num_leds * LED_FRAME_SIZE;
-        self.buffer[end_frame_offset..end_frame_offset + end_frame_size].fill(0x00);
-
-        // Write the data
-        let total_size = START_FRAME_SIZE + num_leds * LED_FRAME_SIZE + end_frame_size;
-        self.spi.write(&self.buffer[..total_size]).await
+        // Write reset signal (fixed size)
+        let reset_signal = [0_u8; 140];
+        self.spi.write(&reset_signal).await
     }
 }
