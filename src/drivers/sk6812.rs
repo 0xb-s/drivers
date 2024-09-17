2df8142e7e3e73b3989c4550561fa70cd01f4eae
0xb-s <145866191+0xb-s@users.noreply.github.com>
2024-09-17 06:19:59 -0700

    Update sk6812.rs

--- src/drivers/sk6812.rs
+++ src/drivers/sk6812.rs
@@ -3,24 +3,22 @@ use crate::drivers::LedDriver;
 use crate::encoding::encode_rgbw8_to_spi_data;
 use crate::encoding::RGBW8;
 use embedded_hal_async::spi::SpiBus;
+use heapless::Vec;
 
-pub struct Sk6812<SPI: SpiBus<u8>> {
+pub struct Sk6812<SPI: SpiBus<u8>, const N: usize> {
     spi: SPI,
     color_order: ColorOrder,
-    buffer: [u8; MAX_BUFFER_SIZE],
+    buffer: Vec<u8, N>,
 }
 
-const MAX_LEDS: usize = 60;
-const MAX_BUFFER_SIZE: usize = MAX_LEDS * 16;
-
-impl<SPI: SpiBus<u8>> Sk6812<SPI> {
+impl<SPI: SpiBus<u8>, const N: usize> Sk6812<SPI, N> {
     /// Creates a new SK6812 driver with the given SPI bus.
     /// Colors default to RGB order.
     pub fn new(spi: SPI) -> Self {
         Self {
             spi,
             color_order: ColorOrder::RGB,
-            buffer: [0; MAX_BUFFER_SIZE],
+            buffer: Vec::new(),
         }
     }
 
@@ -28,26 +26,33 @@ impl<SPI: SpiBus<u8>> Sk6812<SPI> {
     pub fn set_color_order(&mut self, color_order: ColorOrder) {
         self.color_order = color_order;
     }
+
+    /// Initializes the buffer with the required size for the given number of LEDs.
+    fn initialize_buffer(&mut self, num_leds: usize) {
+        let total_size = num_leds * 16;
+
+        // Resize the buffer based on the number of LEDs
+        self.buffer.clear();
+        self.buffer.resize(total_size, 0).ok();
+    }
 }
 
-impl<SPI: SpiBus<u8>> LedDriver<RGBW8> for Sk6812<SPI> {
+impl<SPI: SpiBus<u8>, const N: usize> LedDriver<RGBW8> for Sk6812<SPI, N> {
     type Error = SPI::Error;
 
     async fn write(&mut self, colors: &[RGBW8]) -> Result<(), Self::Error> {
-        let num_leds = core::cmp::min(colors.len(), MAX_LEDS);
-        let data_size = num_leds * 16;
+        let num_leds = colors.len();
+
+        // Initialize the buffer with the appropriate size based on the number of LEDs
+        self.initialize_buffer(num_leds);
 
         // Encode colors into the buffer
-        encode_rgbw8_to_spi_data(
-            &colors[..num_leds],
-            self.color_order,
-            &mut self.buffer[..data_size],
-        );
+        encode_rgbw8_to_spi_data(&colors[..num_leds], self.color_order, &mut self.buffer[..]);
 
         // Write the data to SPI
-        self.spi.write(&self.buffer[..data_size]).await?;
+        self.spi.write(&self.buffer[..]).await?;
 
-        // Write reset signal.
+        // Write reset signal (fixed size)
         let reset_signal = [0_u8; 140];
         self.spi.write(&reset_signal).await
     }
