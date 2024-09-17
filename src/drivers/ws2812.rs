a501d6cff61f95986d116b099b045b5cf99e50ea
0xb-s <145866191+0xb-s@users.noreply.github.com>
2024-09-17 06:20:09 -0700

    Update ws2812.rs

--- src/drivers/ws2812.rs
+++ src/drivers/ws2812.rs
@@ -1,26 +1,25 @@
 use embedded_hal_async::spi::SpiBus;
+use heapless::Vec;
 use smart_leds::RGB8;
 
 use crate::color_order::ColorOrder;
 use crate::drivers::LedDriver;
 use crate::encoding::encode_rgb8_to_spi_data;
-const MAX_LEDS: usize = 60;
-pub struct Ws2812<SPI: SpiBus<u8>> {
+
+pub struct Ws2812<SPI: SpiBus<u8>, const N: usize> {
     spi: SPI,
     color_order: ColorOrder,
-    buffer: [u8; MAX_BUFFER_SIZE],
+    buffer: Vec<u8, N>,
 }
 
-const MAX_BUFFER_SIZE: usize = MAX_LEDS * 12; // Each LED requires 12 bytes
-
-impl<SPI: SpiBus<u8>> Ws2812<SPI> {
+impl<SPI: SpiBus<u8>, const N: usize> Ws2812<SPI, N> {
     /// Creates a new WS2812 driver with the given SPI bus.
     /// Colors default to RGB order.
     pub fn new(spi: SPI) -> Self {
         Self {
             spi,
             color_order: ColorOrder::RGB,
-            buffer: [0; MAX_BUFFER_SIZE],
+            buffer: Vec::new(),
         }
     }
 
@@ -28,24 +27,31 @@ impl<SPI: SpiBus<u8>> Ws2812<SPI> {
     pub fn set_color_order(&mut self, color_order: ColorOrder) {
         self.color_order = color_order;
     }
+
+    /// Initializes the buffer with the required size for the given number of LEDs.
+    fn initialize_buffer(&mut self, num_leds: usize) {
+        let total_size = num_leds * 12; // LED requires 12 bytes to represent its RGB data
+
+        // Resize the buffer based on the number of LEDs
+        self.buffer.clear();
+        self.buffer.resize(total_size, 0).ok(); // Resize the buffer, handle any error silently
+    }
 }
 
-impl<SPI: SpiBus<u8>> LedDriver<RGB8> for Ws2812<SPI> {
+impl<SPI: SpiBus<u8>, const N: usize> LedDriver<RGB8> for Ws2812<SPI, N> {
     type Error = SPI::Error;
 
     async fn write(&mut self, colors: &[RGB8]) -> Result<(), Self::Error> {
-        let num_leds = core::cmp::min(colors.len(), MAX_LEDS);
-        let data_size = num_leds * 12;
+        let num_leds = colors.len();
+
+        // Initialize the buffer with the appropriate size based on the number of LEDs
+        self.initialize_buffer(num_leds);
 
         // Encode colors into the buffer
-        encode_rgb8_to_spi_data(
-            &colors[..num_leds],
-            self.color_order,
-            &mut self.buffer[..data_size],
-        );
+        encode_rgb8_to_spi_data(&colors[..num_leds], self.color_order, &mut self.buffer[..]);
 
         // Write the data to SPI
-        self.spi.write(&self.buffer[..data_size]).await?;
+        self.spi.write(&self.buffer[..]).await?;
 
         // Write reset signal.
         let reset_signal = [0_u8; 140];
